09a7ee61e5e298cda739622ffafc5975f0fb1f13
Vl-Tershch <ddiavall@yandex.ru>
2022-09-19 01:20:09 +0300

    Added JSONs processing from github data

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
 import json
+import requests
 from SecretUser import SecretUser
 from mysql.connector import connect, Error
 
@@ -24,30 +24,42 @@ def getConnection():
         print(e)
 
 def getDataFromDatabase(connection):
-        result_id = []
-        cursor = connection.cursor(buffered=True)
-        cursor.execute("SELECT ИД_ИСУ FROM уп")
-        result_set = cursor.fetchall()
-        for row in result_set:
-            result_id.append(row[0])
-        return result_id
+    result_id = []
+
+    cursor = connection.cursor(buffered=True)
+    cursor.execute("SELECT ИД_ИСУ FROM уп")
+    result_set = cursor.fetchall()
+    for row in result_set:
+        result_id.append(row[0])
+    return result_id
 
 def getDataFromJSON(ids):
     findedNumbers = []
     dataToAdd = []
-    fds = sorted(os.listdir('/Users/vladtereshch/PycharmProjects/OldJsonsData/jsons/'))
+    filenames = []
+
+    pathdirectory = "https://api.github.com/repos/Vl-Tershch/OldJsonsData/git/trees/master?recursive=1"
+    r = requests.get(pathdirectory)
+    res = r.json()
+    for file in res["tree"]:
+        if file["path"].endswith('.json'):
+            help = file["path"].split('/')
+            filenames.append(help[1])
+
+    fds = sorted(filenames)
     for i in fds:
         if i.endswith('.json'):
             filename = i.split('.')
             numberFile = filename[0].split('_')
             if int(numberFile[0]) in ids:
                 findedNumbers.append(int(numberFile[0]))
-                with open('/Users/vladtereshch/PycharmProjects/OldJsonsData/jsons/' + i) as f:
-                    templates = json.load(f)
-                    if 'error_code' in templates.keys():
-                        dataToAdd.append((templates['result']['selection_year'], float(templates['result']['training_period']), numberFile[0]))
-                    else:
-                        dataToAdd.append((templates['selection_year'], float(templates['training_period']), numberFile[0]))
+                url = f'https://raw.githubusercontent.com/Vl-Tershch/OldJsonsData/master/jsons/{i}'
+                resp = requests.get(url)
+                data = json.loads(resp.text)
+                if 'error_code' in data.keys():
+                    dataToAdd.append((data['result']['selection_year'], float(data['result']['training_period']), numberFile[0]))
+                else:
+                    dataToAdd.append((data['selection_year'], float(data['training_period']), numberFile[0]))
     return dataToAdd
 
 def uploadDataToDatabase(connection, data):
@@ -56,6 +68,7 @@ def uploadDataToDatabase(connection, data):
         SET Год = %s, Период = %s 
         WHERE ИД_ИСУ = (%s)
         """
+
     cursor = connection.cursor(buffered=True)
     cursor.executemany(insert_jsons_query, data)
     connection.commit()
